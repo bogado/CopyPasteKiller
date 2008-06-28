c8c09c5fcc087a8f4f2d09f6478285c193da4b33
Victor Bogado <victor@bogado.net>
2008-06-27 23:44:59 -0300

    Test uses the testdb now.

--- c++/test.cpp
+++ c++/test.cpp
@@ -1,12 +1,20 @@
 #include "hash.h"
-#include "file.h"
+#include "filedb.h"
 
 #include <iostream>
 
 int main()
 {
-	analisys::File f("test.cpp");
+	analisys::FileDB fdb;
+	fdb.addFile("test.cpp");
+	fdb.addFile("line.cpp");
+	fdb.addFile("line.h");
 
-	for (int i = 0; i < f.size(); i++)
-		std::cout << f[i] << " |" << f[i].content() <<  "|" << f[i].key() << "\n";
+	for (int i = 0; i < fdb.size(); i++)
+	{
+		std::cout << "\nFile: " << fdb[i] << "\n\n";
+
+		for (int j = 0; j < fdb[i].size(); j++)
+			std::cout << fdb[i][j] << " |" << fdb[i][j].content() <<  "|" << fdb[i][j].key() << "\n";
+	}
 }
