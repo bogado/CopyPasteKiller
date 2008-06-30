3e3e41910ef42d808abdd8122734ea8ea8c90aca
Victor Bogado <victor@bogado.net>
2008-06-30 07:47:24 -0300

    Skeleton for result class.

--- c++/result.h
+++ c++/result.h
@@ -1,12 +1,20 @@
 #ifndef RESULT_H_INCLUDED
 #define RESULT_H_INCLUDED
 
+#include <list>
+
 #include "chunk.h"
 
 namespace analisys {
 
-	class Result : public Chunk
+	class Result
 	{
+	public:
+		void grow(int n);
+
+	private:
+		std::list<Line> Lines;
+		unsigned int size;
 	};
 }
 
