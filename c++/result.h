5d231f5783a47ef61976ece3c075c8f85a934e6d
Victor Bogado <victor@bogado.net>
2008-07-01 17:41:59 -0300

    Rethinking the result object

--- c++/result.h
+++ c++/result.h
@@ -10,46 +10,43 @@ namespace analisys {
 	class Result
 	{
 	public:
+		Result(int len) : len_(len)
+		{}
+
 		void add(const Line &l)
 		{
-			lines_.push_back(l);
+			if (check_(l))
+				lines_.push_back(l);
 		}
 
-		template <class FUNCTOR>
-		void grow(int n, FUNCTOR f)
+		/** grow this result, if possible
+		 *
+		 * This will not grow into a trivial (size < 2) result.
+		 *
+		 * @returns true if it was possible to grow.
+		 */
+		bool grow(Result &res)
 		{
-			Check<FUNCTOR> c(n, f);
-			lines_.remove_if(c);
 		}
 
-		template <class FUNCTOR>
-		bool check(int n, FUNCTOR f) const
+		/// Length of matching blocks
+		unsigned int length() const
 		{
-			Check<FUNCTOR> c(n, f);
-			return std::find_if(lines_.begin(), lines_.end(), c) != lines_.end();
+			return len_;
 		}
+
+		/// Number of lines in this result
+		unsigned int size() const
+		{
+			return lines_.size();
+		}
+
 	private:
 		std::list<Line> lines_;
-		unsigned int size_;
+		unsigned int len_;
 
-		template<class FUNCTOR>
-		class Check
-		{
-		public:
-			Check(int n, FUNCTOR f) : n_(n), f_(f)
-			{}
-
-			bool operator()(const Line &l)
-			{
-				if (l.valid(n_) && f_(l + n_))
-					return true;
-
-				return false;
-			}
-		private:
-			int n_;
-			FUNCTOR f_;
-		};
+		/// Check if this line belongs to this result.
+		virtual bool check_(const Line &l) const = 0
 	};
 }
 
